81aada1a3fe0558bca278c6685b7b2eb4c23be55
Tharun <tharunvijay836@gmail.com>
2024-02-16 15:22:46 +0530

    Day37:object in the frame sending to the mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import cv2
 import time
+import glob
+import os
 from emailing import send_email
 
 video = cv2.VideoCapture(0)
@@ -7,6 +9,14 @@ time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
 
 while True:
     status = 0
@@ -27,17 +37,25 @@ while True:
 
     for contour in contours:
         if cv2.contourArea(contour) < 5000:
-            continue
+             continue
         x, y, w, h = cv2.boundingRect(contour)
         rectangle = cv2.rectangle(frame, (x, y), (x+w,y+h),(0,255,0), 3)
         if rectangle.any():
             status = 1
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index=int(len(all_images) / 2)
+            image_with_object = all_images[index]
+
 
     status_list.append(status)
     status_list=status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        send_email(image_with_object)
+        clean_folder()
+
     print(status_list)
 
 
